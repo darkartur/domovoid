32bf21b9b244e675aa426a0ac6f9c04e013a0610
artur.udalov <artur.udalov@gmail.com>
2026-03-04 22:56:26 +0100

    Moves directory definition to core

--- packages/plugin-utils/index.ts
+++ packages/plugin-utils/index.ts
@@ -1,4 +1,14 @@
-export * from "./constants.ts";
+export * from "../core/constants.ts";
+
+// Types
+export type * from "./types/agent.ts";
+export type * from "./types/all-capabilities.ts";
+export type * from "./types/any-event.ts";
+export type * from "./types/context.ts";
+export type * from "./types/events.ts";
+export type * from "./types/plugin.ts";
 export type * from "./types/tasks.ts";
 export type * from "./types/vcs.ts";
-export type * from "./types/agent.ts";
+
+// Utils
+export { default as definePlugin } from "./utils/define-plugin.ts";
---
+++ packages/plugin-utils/types/any-event.ts
@@ -0,0 +1,3 @@
+import type { TasksEvent } from "./tasks.ts";
+
+export type AnyEvent = TasksEvent;
