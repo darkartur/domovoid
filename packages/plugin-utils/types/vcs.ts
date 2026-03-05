71fdd378818e1b691590287113b23b902a77ab46
artur.udalov <artur.udalov@gmail.com>
2026-03-05 07:58:16 +0100

    Adds extra capabilties necessary for the test

--- packages/plugin-utils/types/vcs.ts
+++ packages/plugin-utils/types/vcs.ts
@@ -4,4 +4,5 @@ interface VcsCloneOptions {
 
 export interface VcsCapabilities {
   clone(repositoryUrl: string, directoryPath: string, options: VcsCloneOptions): Promise<void>;
+  remove(directoryPath: string): Promise<void>;
 }
