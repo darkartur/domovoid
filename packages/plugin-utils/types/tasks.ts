cfb817dd089e5899242f6195dba22a4a67cf0673
artur.udalov <artur.udalov@gmail.com>
2026-03-05 07:58:16 +0100

    Adds extra capabilties necessary for the test

--- packages/plugin-utils/types/tasks.ts
+++ packages/plugin-utils/types/tasks.ts
@@ -3,6 +3,7 @@ import type { PluginEvent } from "./events.ts";
 export interface TasksCapabilities {
   getActiveTasks(projectId: string): Promise<Task[]>;
   getProjects(): Promise<TaskProject[]>;
+  postComment(taskId: string, result: string): Promise<void>;
 }
 
 export interface TaskProject {
